32d3999d3a353b2141eb928974c77fb01106879d
Manas Singh <manas.singh@walmart.com>
2021-10-27 18:31:37 +0530

    master code(boilerplate)

--- src/reducer/index.jsx
+++ src/reducer/index.jsx
@@ -1,10 +1,8 @@
 import { createStore, applyMiddleware, combineReducers } from "redux";
 import thunk from "redux-thunk";
 import clock from "./clock/";
-import tree from "./tree/";
 const rootReducer = combineReducers({
-  clock,
-  tree,
+  clock
 })
 const store = createStore(rootReducer, {}, applyMiddleware(thunk));
 export default store;
